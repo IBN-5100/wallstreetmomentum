3bf2fb8589a54a697592b2ffa89c66eac389f6c3
Ricky Deng <43194683+IBN-5100@users.noreply.github.com>
2024-09-26 03:27:24 +0000

    psa

--- app/signin/[id]/page.tsx
+++ app/signin/[id]/page.tsx
@@ -101,8 +101,7 @@ export default async function SignIn({
             viewProp !== 'signup' &&
             allowOauth && (
               <>
-                <Separator text="Third-party sign-in" />
-                
+                <Separator text="discord/google soon" />
               </>
             )}
         </Card>
